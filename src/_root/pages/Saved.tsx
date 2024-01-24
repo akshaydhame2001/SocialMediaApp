418758240ed11e729b9df109d431f2e414968eff
Harshal <akshaydhame2001@gmail.com>
2024-01-24 11:43:12 +0530

    Saved Posts

--- src/_root/pages/Saved.tsx
+++ src/_root/pages/Saved.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import GridPostList from "@/components/shared/GridPostList";
+import Loader from "@/components/shared/Loader";
+import { useGetCurrentUser } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
 
 const Saved = () => {
-  return <div>Saved</div>;
+  const { data: currentUser } = useGetCurrentUser();
+
+  const savePosts = currentUser?.save.map((savePost: Models.Document) => ({
+    ...savePost.post,
+    creator: {
+      imageUrl: currentUser.imageUrl,
+    },
+  }));
+  return (
+    // flex flex-col flex-1 items-center gap-10 overflow-scroll py-10 px-5 md:p-14 custom-scrollbar;
+    <div className="saved-container">
+      <div className="flex gap-2 w-full max-w-5xl">
+        <img
+          src="/assets/icons/save.svg"
+          width={36}
+          height={36}
+          alt="save"
+          className="invert-white"
+        />
+        <h2 className="h3-bold md:h2-bold w-full">Saved Posts</h2>
+      </div>
+
+      {!currentUser ? (
+        <Loader />
+      ) : (
+        <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+          {savePosts.length === 0 ? (
+            <p className="text-light-4">No available posts</p>
+          ) : (
+            <GridPostList posts={savePosts} showStats={false} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Saved;
