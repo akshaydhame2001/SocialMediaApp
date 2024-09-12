e7fa7a1af899f5a8656cc2f5321a2e95a7975671
Harshal <akshaydhame2001@gmail.com>
2024-09-12 16:06:13 +0530

    commit

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -38,7 +38,7 @@ const LeftSidebar = () => {
           </div>
         </Link>
 
-        <ul className="flex flex-col gap-6">
+        <ul className="flex flex-col gap-2">
           {sidebarLinks.map((link: INavLink) => {
             const isActive = pathname === link.route;
 
